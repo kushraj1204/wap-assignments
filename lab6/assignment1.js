7c5d0eac25e240a2c386ee0c1433c5aefeece538
Kush Raj Rimal <37412830+kushraj1204@users.noreply.github.com>
2024-05-04 15:02:48 -0500

    Added lab 6 assignment1 and assignment2

---
+++ lab6/assignment1.js
@@ -0,0 +1,16 @@
+function b() {
+    const y = 30;
+    a();
+    function a() {
+        console.log(x, y);
+    }
+    var x = 10;
+    console.log(x, y);
+}
+const x = 20;
+var y = 40;
+b();
+
+// Output:
+// undefined 30
+// 10 30
